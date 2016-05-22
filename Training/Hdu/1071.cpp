c4c67c383ebd9bb1f5efffd96480efd696562487
Voleking <volekingsg@gmail.com>
2016-05-22 20:00:51 +0800

    BJTU points race 3

---
+++ Training/Hdu/1071.cpp
@@ -0,0 +1,54 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+typedef long l;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned long ul;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 50005;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+int main(int argc, char const *argv[])
+{
+	int n;
+	double a, k, area, x1, x2, x3, y1, y2, y3;
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> x1 >> y1;
+		cin >> x2 >> y2;
+		cin >> x3 >> y3;
+		a = (y2 - y1) / sqr(x2 - x1);
+		k = (y3 - y2) / (x3 - x2);
+		area = a * (x3 * x3 * x3 - x2 * x2 * x2) / 3 - a * x1 * (sqr(x3) - sqr(x2)) + (a * sqr(x1) + y1 - y2 + k * x2) * (x3 - x2) -
+		k * (sqr(x3) - sqr(x2)) / 2;
+		printf("%.2lf\n", area);
+	}
+	return 0;
+}
---
+++ Training/Hdu/1969.cpp
@@ -0,0 +1,71 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+typedef long l;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned long ul;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double Pi = acos(-1.0);
+const double eps = 1e-6;
+const int INF = INT_MAX;
+const int MAX_N = 1e4 + 5;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+int t, n, f;
+double sum = 0, a[MAX_N];
+bool cal(double x) {
+	int num = 0;
+	for (int i = 0; i < n; ++i) {
+		num += (int)(a[i] / x);
+		if (num >= f)
+			return true;
+	}
+	return false;
+}
+int main(int argc, char const *argv[])
+{
+	cin >> t;
+	while (t--) {
+		cin >> n >> f;
+		++f;
+		for (int i = 0; i < n; ++i) {
+			cin >> a[i];
+			a[i] = Pi * sqr(a[i]);
+			sum += a[i];
+		}
+		double l = 0, r = sum, mid;
+		while (fabs(r - l) > eps) {
+			mid = (l + r) / 2;
+			if (!cal(mid)) r = mid; 
+			else l = mid;
+		}
+		printf("%.4f\n", l);
+	}
+	return 0;
+}
