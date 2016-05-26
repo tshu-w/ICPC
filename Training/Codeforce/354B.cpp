8303346e81476d4a4de035a4b2ea28090063f505
Voleking <volekingsg@gmail.com>
2016-05-26 10:18:40 +0800

    Codeforces 354 A B C

---
+++ Training/Codeforce/354B.cpp
@@ -0,0 +1,60 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+typedef long l;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned long ul;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 50005;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+
+int main(int argc, char const *argv[])
+{
+	int n, t, cnt = 0;
+	double a[15][15];
+	cin >> n >> t;
+	memset(a, 0, sizeof a);
+	a[0][0] = t;
+	for (int j = 0; j < n; ++j)
+		for (int k = 0; k <= j; ++k) {
+			if (a[j][k] > 1. - 1e-6) {
+				a[j + 1][k] += (a[j][k] - 1) / 2.0;
+				a[j + 1][k + 1] += (a[j][k] - 1) / 2.0;
+			}
+		}
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j <= i; ++j)
+			if (a[i][j] > 1. - 1e-6)
+				++cnt;
+	cout << cnt << endl;
+	return 0;
+}
