a9078d8a50d663e242d96c65c341f242a445f561
Voleking <volekingsg@gmail.com>
2016-06-05 20:01:15 +0800

    2016 TCO Beijing

---
+++ Training/TopCoder/2016-Beijing/250.cpp
@@ -0,0 +1,60 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+typedef long l;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned long ul;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 505;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+class GroupSRMDiv2
+{
+public:
+	int FindGroups(vector <int> t) {
+		int flag[MAX_N];
+		memset(flag, 0, sizeof flag);
+		for (int i = 0; i < t.size(); ++i)
+			++flag[t[i]];
+		int ans = 0;
+		for (int i = 1; i < MAX_N; ++i)
+			if (flag[i] % i == 0) 
+				ans += flag[i] / i;
+			else 
+				return -1;
+		return ans;
+	}
+};
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
