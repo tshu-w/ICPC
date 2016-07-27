ce0c4a19f181db4ed297fb291832e9ee8106191a
Voleking <volekingsg@gmail.com>
2016-07-27 20:45:13 +0800

    BJTU Div 1 Training 3

---
+++ Training/Codeforces/276D.cpp
@@ -0,0 +1,60 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+// #define __DEBUG__
+#ifdef __DEBUG__
+	#define DEBUG(...) printf(__VA_ARGS__)
+#else
+	#define DEBUG(...)
+#endif	
+#define filename ""
+#define setfile() freopen(filename".in", "r", stdin); freopen(filename".out", "w", stdout);
+
+using namespace std;
+
+typedef long l;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned long ul;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 50005;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+int main(int argc, char const *argv[])
+{
+	ll l, r, ans;
+	cin >> l >> r;
+	int i;
+	for (i = 63; i >= 0; i--)
+		if ((l & (1LL << i)) ^ (r & (1LL << i)))
+			break;
+	ans = (1LL << (i + 1)) - 1;
+	cout << ans << endl;
+	return 0;
+}
