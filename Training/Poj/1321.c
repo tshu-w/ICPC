af0033e24d523185b8d0d711e813cd11e2c5cf95
Voleking <volekingsg@gmail.com>
2016-07-27 17:44:19 +0800

    BJTU Div 2 Training 3

--- Training/Poj/1321.c
+++ Training/Poj/1321.c
@@ -1,33 +1,77 @@
-#include <stdio.h>
-#include <string.h>
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
 
-int column[10], n, k;
-char board[10][10];
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+// #define __DEBUG__
+#ifdef __DEBUG__
+	#define DEBUG(...) printf(__VA_ARGS__)
+#else
+	#define DEBUG(...)
+#endif	
+#define filename ""
+#define setfile() freopen(filename".in", "r", stdin); freopen(filename".out", "w", stdout);
 
-int find(int num, int line) {
-	if (num > k) {
-		return 1;
+using namespace std;
+
+typedef long l;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned long ul;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 10;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+int N, K, ans;
+bool line[MAX_N];
+char maze[MAX_N][MAX_N];
+void dfs(int k, int cur) {
+	if (k == K) {
+		++ans;
+		return ;
 	}
-	if (line > n)
-		return 0;
-	int sum = 0;
-			for (int j = 0; j < n; j++)
-				if (!column[j] && board[line][j] == '#') {
-						column[j] = 1;
-						sum += find(num + 1, line + 1);
-						column[j] = 0;
-					}
-	sum += find(num, line + 1);
-	return sum;
-};
+	int res = 0;
+	for (int i = cur; i < N; ++i)
+		for (int j = 0; j < N; ++j)
+			if (maze[i][j] == '#' && !line[j]) {
+				line[j] = true;
+				dfs(k + 1, i + 1);
+				line[j] = false;
+			}
+}
 int main(int argc, char const *argv[])
- {
- 	while(scanf("%d%d", &n, &k) && n != -1 && k != -1) {
- 		memset(column, 0, sizeof(column));
- 		memset(board, 0, sizeof(board));
- 		for (int i = 0; i < n; i++)
- 			scanf("%s", board[i]);
- 		printf("%d\n", find(1, 0));
- 	}
- 	return 0;
-} 
+{
+	while (~scanf("%d%d%*c", &N, &K) && N != -1 && K != -1) {
+		for (int i = 0; i < N; ++i) 
+			gets(maze[i]);
+		ans = 0;
+		memset(line, false, sizeof line);
+		dfs(0, 0);		
+		printf("%d\n", ans);
+	}
+	return 0;
+}
