093c3bc16f0e4ea2bab8839fbab409e0537c644f
Voleking <volekingsg@gmail.com>
2016-08-09 19:14:33 +0800

    2016 MUTC 7

---
+++ Training/HDU/5810.cpp
@@ -0,0 +1,64 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+// #define __DEBUG__
+#ifdef __DEBUG__
+    #define DEBUG(...) printf(__VA_ARGS__)
+#else
+    #define DEBUG(...)
+#endif  
+#define filename ""
+#define setfile() freopen(filename".in", "r", stdin); freopen(filename".out", "w", stdout);
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 50005;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+ll n, m;
+
+ll gcd( ll a, ll b ) {
+    if( b == 0 ) return a;
+    return gcd( b, a % b );
+}
+
+int main() {
+    while( ~scanf( "%I64d%I64d", &n, &m ) && n + m ) {
+        ll ta = n * ( m - 1 );
+        ll tb = m * m;
+        if( ta == 0 ) { puts( "0/1" ); continue; }
+        ll tt = gcd( ta, tb );
+        ta /= tt; tb /= tt;
+        printf( "%I64d/%I64d\n", ta, tb );
+    }
+    return 0;
+}
