4b560a488eb53c746b6e2383b03fbf3430e180c0
Voleking <volekingsg@gmail.com>
2016-08-12 12:13:28 +0800

    CPDC §2 exersices

---
+++ Training/POJ/3268.cpp
@@ -0,0 +1,90 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+// #define __DEBUG__
+#ifdef __DEBUG__
+	#define DEBUG(...) printf(__VA_ARGS__)
+#else
+	#define DEBUG(...)
+#endif	
+#define filename ""
+#define setfile() freopen(filename".in", "r", stdin); freopen(filename".out", "w", stdout);
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_V = 1000 + 10;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+struct edge {
+	int from, to, cost;
+};
+vector<edge> G[MAX_V];
+int dist[MAX_V], d[MAX_V][MAX_V];
+
+int V, E, X;
+void dijkstra(int s) {
+    priority_queue<Pii, vector<Pii>, greater<Pii> > que;// fisrt 是最短距离，second 是顶点编号
+    fill(dist, dist + V, INF);
+    dist[s] = 0; que.push(Pii(0, s));
+    while (!que.empty()) {
+        Pii p = que.top(); que.pop();
+        int v = p.second;
+        if (dist[v] < p.first) continue;
+        for (int i = 0; i < G[v].size(); i++) {
+            edge e = G[v][i];
+            if (dist[e.to] > dist[v] + e.cost) {
+                dist[e.to] = dist[v] + e.cost;
+                que.push(Pii(dist[e.to], e.to));
+            }
+        }
+    } 
+}
+
+int main(int argc, char const *argv[])
+{
+	scanf("%d%d%d", &V, &E, &X); --X;	
+	for (int i = 0, v, u, x; i < E; ++i) {
+		scanf("%d%d%d", &v, &u, &x);
+		--v, --u;
+		G[v].push_back(edge{v, u, x});
+	}
+	for (int i = 0; i < V; ++i) {
+		dijkstra(i);
+		for (int j = 0; j < V; ++j)
+			d[i][j] = dist[j];
+	}
+	int ans = 0;
+	for (int i = 0; i < V; ++i)
+		ans = max(ans, d[i][X] + d[X][i]);
+	printf("%d\n", ans);
+	return 0;
+}
---
+++ Training/POJ/3669.cpp
@@ -0,0 +1,93 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+// #define __DEBUG__
+#ifdef __DEBUG__
+	#define DEBUG(...) printf(__VA_ARGS__)
+#else
+	#define DEBUG(...)
+#endif	
+#define filename ""
+#define setfile() freopen(filename".in", "r", stdin); freopen(filename".out", "w", stdout);
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 50000 + 10;
+const int MAX_W = 500 + 10;
+const int dx[] = {0, 1, 0, -1, 0};
+const int dy[] = {0, 0, 1, 0, -1};
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+int M, X, Y, T, G[MAX_W][MAX_W];
+bool vis[MAX_W][MAX_W];
+
+bool check(int x, int y) {
+	return (0 <= x && x < MAX_W && 0 <= y && y < MAX_W);
+}
+int main(int argc, char const *argv[])
+{
+	memset(G, -1, sizeof G);
+	memset(vis, 0, sizeof vis);
+	scanf("%d", &M);
+	for (int i = 0; i < M; ++i) {
+		scanf("%d%d%d", &X, &Y, &T);
+		for (int d = 0; d < 5; ++d) {
+			int xx, yy;
+			xx = X + dx[d]; yy = Y + dy[d];
+			if (check(xx, yy) && (G[xx][yy] == -1 || G[xx][yy] > T))
+				G[xx][yy] = T;
+		}
+	}
+	queue<pair<Pii, int> > q;
+	if (check(0, 0) && (G[0][0] == -1 || G[0][0] > 0)) {
+		q.push(make_pair(Pii(0, 0), 0));
+		vis[0][0] = true;
+	}
+	while (q.size()) {
+		pair<Pii, int> p = q.front(); q.pop();
+		if (G[p.first.first][p.first.second] == -1) {
+			printf("%d\n", p.second);
+			// cout << p.first.first << " " << p.first.second << endl;
+			return 0;
+		}	
+		for (int i = 1; i < 6; ++i) {
+			int xx, yy;
+			xx = p.first.first + dx[i]; yy = p.first.second + dy[i];
+			if (check(xx, yy) && !vis[xx][yy] && (G[xx][yy] == -1 || p.second + 1 < G[xx][yy])) {
+				q.push(make_pair(Pii(xx, yy), p.second + 1));
+				vis[xx][yy] = true;	
+			}
+		}
+	}
+	printf("-1\n");
+	return 0;
+}
