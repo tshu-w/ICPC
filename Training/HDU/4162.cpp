73d43258cf176ae1def8d1cd58ac1ec37eb917df
Voleking <volekingsg@gmail.com>
2016-08-18 10:56:25 +0800

    BJTU Div. 2 Training 14

---
+++ Training/HDU/4162.cpp
@@ -0,0 +1,79 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+// #define __DEBUG__
+#ifdef __DEBUG__
+	#define DEBUG(...) printf(__VA_ARGS__)
+#else
+	#define DEBUG(...)
+#endif	
+#define filename ""
+#define setfile() freopen(filename".in", "r", stdin); freopen(filename".out", "w", stdout);
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 300000 + 10;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+char s[MAX_N];
+int len;
+
+int getMinString(char s[], int len) {  
+    int i = 0, j = 1, k = 0;  
+    while(i < len && j < len && k < len) {
+        int t = s[(i+k)%len] - s[(j+k)%len];
+        if(t == 0) k++;
+        else {
+            if(t > 0) i += k + 1;//getMaxString: t < 0
+            else j += k + 1;
+            if(i==j) j++;
+            k = 0;
+        }
+    }
+    return min(i, j);
+}
+
+int main(int argc, char const *argv[])
+{
+	while (~scanf("%s", s)) {
+		len = strlen(s);
+		s[len] = s[0];
+		for (int i = 0; i < len; ++i)
+			s[i] = (s[i + 1] - s[i] + 8) % 8 + '0';
+		s[len] = 0;
+		int pos = getMinString(s, len);
+		printf("%s", s + pos);
+		s[pos] = 0;
+		puts(s);
+	}
+	return 0;
+}
---
+++ Training/POJ/3461.cpp
@@ -0,0 +1,88 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(0); std::cout.tie(0);
+// #define __DEBUG__
+#ifdef __DEBUG__
+	#define DEBUG(...) printf(__VA_ARGS__)
+#else
+	#define DEBUG(...)
+#endif	
+#define filename ""
+#define setfile() freopen(filename".in", "r", stdin); freopen(filename".out", "w", stdout);
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 1e6 + 10;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+int t;
+string s1, s2;
+
+int nxt[MAX_N];
+void getNext(const string &str) {
+    int len = str.length();
+    int j = 0, k;
+    k = nxt[0] = -1;
+    while (j < len) {
+        if (k == -1 || str[j] == str[k]) 
+            nxt[++j] = ++k;
+        else k = nxt[k];
+    }
+}
+int kmp(const string &tar, const string &pat) {
+    getNext(pat);
+    int num, j, k;
+    int lenT = tar.length(), lenP = pat.length();
+    num = j = k = 0;
+    while (j < lenT) {
+        if(k == -1 || tar[j] == pat[k])
+            j++, k++;
+        else k = nxt[k];
+        if(k == lenP) {
+            // res = max(res, j - lenP);
+            k = nxt[k];
+            ++num;
+        }
+    }
+    return num;//lenP - res - 1;
+}
+
+int main(int argc, char const *argv[])
+{
+	IOS;
+	cin >> t;
+	while (t--) {
+		cin >> s1 >> s2;
+		cout << kmp(s2, s1) << endl;
+	}
+	return 0;
+}
