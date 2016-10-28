021bee5db855d4e212a0dad893bb47bbc283addc
Voleking <volekingsg@gmail.com>
2016-10-28 20:26:37 +0800

    CPDC §3 Fanzhuan

---
+++ Training/POJ/1222_2.cpp
@@ -0,0 +1,100 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <bitset>
+
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+// #define __DEBUG__
+#ifdef __DEBUG__
+	#define DEBUG(...) printf(__VA_ARGS__)
+#else
+	#define DEBUG(...)
+#endif
+#define filename ""
+#define setfile() freopen(filename".in", "r", stdin); freopen(filename".ans", "w", stdout);
+#define resetfile() freopen("/dev/tty", "r", stdin); freopen("/dev/tty", "w", stdout); system("more " filename".ans");
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 10;
+const int N = 5, M = 6;
+
+const int dx[5] = {-1, 0, 0, 0, 1};
+const int dy[5] = {0, -1, 0, 1, 0};
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+int t, tt, title[MAX_N][MAX_N], opt[MAX_N][MAX_N];
+
+int get(int x, int y) {
+	int c = title[x][y];
+	for (int d = 0; d < 5; ++d) {
+		int xx, yy;
+		xx = x + dx[d]; yy = y + dy[d];
+		if (0 <= xx && xx < N && 0 <= yy && yy < M)
+			c += opt[xx][yy];
+	}
+	return c % 2;
+}
+
+bool cal() {
+	for (int i = 1; i < N; ++i)
+		for (int j = 0; j < M; ++j)
+			if (get(i - 1, j)) opt[i][j] = 1;
+	for (int j = 0; j < M; ++j)
+		if (get(N - 1, j)) return false;
+	return true;
+}
+
+void solve() {
+	for (int i = 0; i < 1 << M; ++i) {
+		memset(opt, 0, sizeof opt);
+		for (int j = 0; j < M; ++j) {
+			opt[0][M - j - 1] = i >> j & 1;
+		}
+		if (cal())
+			break;
+	}
+	for (int i = 0; i < N; ++i)
+		for (int j = 0; j < M; ++j)
+			printf("%d%c", opt[i][j], (j + 1 == M)? '\n' : ' ');
+}
+
+int main(int argc, char const *argv[])
+{
+	scanf("%d", &t);
+	while (t--) {
+		printf("PUZZLE #%d\n", ++tt);
+		for (int i = 0; i < N; ++i)
+			for (int j = 0; j < M; ++j)
+				scanf("%d", &title[i][j]);
+		solve();
+	}
+	return 0;
+}
---
+++ Training/POJ/3185.cpp
@@ -0,0 +1,76 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <bitset>
+
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+// #define __DEBUG__
+#ifdef __DEBUG__
+	#define DEBUG(...) printf(__VA_ARGS__)
+#else
+	#define DEBUG(...)
+#endif
+#define filename ""
+#define setfile() freopen(filename".in", "r", stdin); freopen(filename".ans", "w", stdout);
+#define resetfile() freopen("/dev/tty", "r", stdin); freopen("/dev/tty", "w", stdout); system("more " filename".ans");
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 30;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+int N = 20, ans = INF, A[MAX_N], B[MAX_N];
+
+int cal() {
+	int res = 0;
+	for (int i = 1; i < N; ++i)
+		if (A[i - 1]) {
+			++res;
+			for (int j = i - 1; j <= i + 1; ++j)
+				if (j < N)
+					A[j] = (A[j] + 1) % 2;
+		}
+	if (!A[N - 1]) return res;
+	else return INF;
+}
+
+int main(int argc, char const *argv[])
+{
+	for (int i = 0; i < N; ++i)
+		scanf("%d", A + i);
+	memcpy(B, A, sizeof A);
+	ans = min(cal(), ans);
+	memcpy(A, B, sizeof A);
+	A[0] = (A[0] + 1) % 2;
+	A[1] = (A[1] + 1) % 2;
+	ans = min(cal() + 1, ans);
+	printf("%d\n", ans);
+	return 0;
+}
