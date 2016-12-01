1f1d454d28cf20b021de73a01fdd86c074599b16
Voleking <volekingsg@gmail.com>
2016-12-01 14:43:36 +0800

    2015 ACM/ICPC EC-Finals

---
+++ Training/UVALive/7500.cpp
@@ -0,0 +1,66 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <bitset>
+
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+// #define __DEBUG__
+#ifdef __DEBUG__
+	#define DEBUG(...) printf(__VA_ARGS__)
+#else
+	#define DEBUG(...)
+#endif
+#define filename ""
+#define setfile() freopen(filename".in", "r", stdin); freopen(filename".ans", "w", stdout);
+#define resetfile() freopen("/dev/tty", "r", stdin); freopen("/dev/tty", "w", stdout); system("more " filename".ans");
+
+using namespace std;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 1e6 + 10;
+
+int T;
+ll N;
+
+int main(int argc, char const *argv[])
+{
+	scanf("%d", &T);	
+	for (int i = 1; i <= T; ++i) {
+		scanf("%lld", &N);
+		ll lb = 1, ub = sqrt(2 * N) + 1;
+		while (lb + 1 < ub) {
+			ll mid = (lb + ub) / 2;
+			if (mid * (mid + 1) / 2 <= N) lb = mid;
+			else ub = mid;
+		}
+		printf("Case #%d: %lld\n", i, lb * (lb + 1) / 2);
+	}
+	return 0;
+}
---
+++ Training/UVALive/7505.cpp
@@ -0,0 +1,89 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <bitset>
+
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+// #define __DEBUG__
+#ifdef __DEBUG__
+	#define DEBUG(...) printf(__VA_ARGS__)
+#else
+	#define DEBUG(...)
+#endif
+#define filename ""
+#define setfile() freopen(filename".in", "r", stdin); freopen(filename".ans", "w", stdout);
+#define resetfile() freopen("/dev/tty", "r", stdin); freopen("/dev/tty", "w", stdout); system("more " filename".ans");
+
+using namespace std;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 1e6 + 10;
+const int MOD = 1e9 + 7;
+
+int t, tt, N, K;
+ll pw2[MAX_N], dp[MAX_N], sum[MAX_N];
+
+void init() {
+	pw2[0] = 1;
+	for (int i = 1; i < MAX_N; ++i) {
+		pw2[i] = (pw2[i - 1] * 2) % MOD;
+		sum[i] = sum[i - 1] + i;
+	}
+}
+
+ll get(int K) {
+	for (int i = K; i; --i)
+		if (2 * sum[i] < sum[K]) {
+			return i;
+		}
+	return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+	scanf("%d", &t);
+	init();
+	while (t--) {
+		scanf("%d%d", &N, &K);
+		dp[K] = pw2[get(K)];
+		ll tmp = dp[K];
+		int j = K;
+		for (int i = K + 1; i <= N; ++i) {
+			while (2 * sum[j] < sum[i]) {
+				tmp -= dp[j];
+				if (tmp < 0) tmp += MOD;
+				++j;
+			}
+			dp[i] = tmp % MOD; tmp = (tmp + dp[i]) % MOD;
+		}
+		printf("Case #%d: %lld\n", ++tt, dp[N] * 2 % MOD);
+	}	
+	return 0;
+}
