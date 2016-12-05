de1efb86ec7cb8ff0b0202b8ed99a2eea28467a8
Voleking <volekingsg@gmail.com>
2016-12-05 19:05:43 +0800

    CPDC §3 Segment tree & sqrt decomposition

---
+++ Training/POJ/2104.cpp
@@ -0,0 +1,145 @@
+//#include <bits/stdc++.h>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <complex>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <bitset>
+
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+// #define __DEBUG__
+#ifdef __DEBUG__
+	#define DEBUG(...) printf(__VA_ARGS__)
+#else
+	#define DEBUG(...)
+#endif
+#define filename ""
+#define setfile() freopen(filename".in", "r", stdin); freopen(filename".ans", "w", stdout);
+#define resetfile() freopen("/dev/tty", "r", stdin); freopen("/dev/tty", "w", stdout); system("more " filename".ans");
+
+using namespace std;
+
+template <typename T>
+inline T sqr(T a) { return a * a;};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int > Pii;
+
+const double pi = acos(-1.0);
+const int INF = INT_MAX;
+const int MAX_N = 1e5 + 10;
+
+
+const int ST_SIZE = 1 << 18;
+
+int N, M, l, r, k, A[MAX_N], nums[MAX_N];
+vector<int> data[ST_SIZE];
+
+void init(int k, int l, int r) {
+	if (l + 1 == r)
+		data[k].push_back(A[l]);
+	else {
+		int lch = 2 * k + 1, rch = 2 * k + 2;
+		init(lch, l, (l + r) / 2);
+		init(rch, (l + r) / 2, r);
+		data[k].resize(r - l);
+		merge(data[lch].begin(), data[lch].end(), data[rch].begin(), data[rch].end(), data[k].begin());
+	}
+}
+
+int query(int a, int b, int x, int k, int l, int r) {
+	if (b <= l || r <= a)
+		return 0;
+	else 
+		if (a <= l && r <= b) 
+			return upper_bound(data[k].begin(), data[k].end(), x) - data[k].begin();
+		else 
+			return query(a, b, x, 2 * k + 1, l, (l + r) / 2) + query(a, b, x, 2 * k + 2, (l + r) / 2, r);
+}
+
+int main(int argc, char const *argv[])
+{
+	scanf("%d%d", &N, &M);
+	for (int i = 0; i < N; ++i) {
+		scanf("%d", A + i);
+		nums[i] = A[i];
+	}
+	sort(nums, nums + N);
+	init(0, 0, N);
+
+	for (int i = 0; i < M; ++i) {
+		scanf("%d%d%d", &l, &r, &k); --l;
+		int lb = -1, ub = N - 1;
+		while (lb + 1 < ub) {
+			int mid = (lb + ub) / 2;
+			if (query(l, r, nums[mid], 0, 0, N) >= k) ub = mid;
+			else lb = mid;
+		}
+		printf("%d\n", nums[ub]);
+	}
+
+	return 0;
+}
+
+
+// const int B = 1000;
+
+// int N, M, l, r, k;
+// int A[MAX_N], nums[MAX_N];
+// vector<int> bucket[MAX_N / B];
+
+// int main(int argc, char const *argv[])
+// {
+// 	scanf("%d%d", &N, &M);
+// 	for (int i = 0; i < N; ++i) {
+// 		scanf("%d", A + i);
+// 		bucket[i / B].push_back(A[i]);
+// 		nums[i] = A[i];
+// 	}
+// 	sort(nums, nums + N);
+// 	for (int i = 0; i < N / B; ++i)
+// 		sort(bucket[i].begin(), bucket[i].end());
+// 	for (int i = 0; i < M; ++i) {
+// 		scanf("%d%d%d", &l, &r, &k); --l;
+
+// 		int lb = -1, ub = N - 1;
+// 		while (lb + 1 < ub) {
+// 			int mid = (lb + ub) / 2;
+// 			int x = nums[mid];
+// 			int tl = l, tr = r, c = 0;
+
+// 			while (tl < tr && tl % B != 0) if (A[tl++] <= x) ++c;
+// 			while (tl < tr && tr % B != 0) if (A[--tr] <= x) ++c;
+
+// 			while (tl < tr) {
+// 				int b = tl / B;
+// 				c += upper_bound(bucket[b].begin(), bucket[b].end(), x) - bucket[b].begin();
+// 				tl += B;
+// 			}
+
+// 			if (c >= k) ub = mid;
+// 			else lb = mid;
+// 		}
+
+// 		printf("%d\n", nums[ub]);
+// 	}
+
+// 	return 0;
+// }
--- Training/POJ/3368.cpp
+++ Training/POJ/3368.cpp
@@ -39,55 +39,111 @@ typedef pair<int, int > Pii;
 
 const double pi = acos(-1.0);
 const int INF = INT_MAX;
-const int MAX_N = 1e5 + 10;
+const int MAX_N = 1 << 17;
 
 template <typename T>
 inline T sqr(T a) { return a * a;};
 
-int n, q, A[MAX_N], st[MAX_N][20];
+struct Node
+{
+	int lc, rc, c;
+};
 
-void ST() {
-	for (int i = 1; i <= n; ++i) {
-		if (i == 1 || A[i] != A[i - 1]) st[i][0] = 1;
-		else st[i][0] = st[i - 1][0] + 1;
-		// printf("%d ", st[i][0]);
-	}
-	// printf("\n");
-	int k = log(double(n + 1)) / log(2.);
-	for (int j = 1; j <= k; ++j) {
-		for (int i = 1; i + (1 << j) - 1 <= n; ++i) {
-			st[i][j] = max(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
-			// printf("%d ", st[i][j]);
+int n, q, A[MAX_N], l, r;
+Node dat[2 * MAX_N];
+
+void build(int k, int l, int r) {
+	if (l + 1 == r)
+		dat[k].lc = dat[k].rc = dat[k].c = 1;
+	else {
+		int lch = 2 * k + 1, rch = 2 * k + 2, mid = (l + r) / 2;
+		build(lch, l, mid);
+		build(rch, mid, r);
+		if (A[mid - 1] == A[mid]) {
+			dat[k].c = dat[lch].rc + dat[rch].lc;
+			if (dat[lch].lc == mid - l) dat[k].lc = dat[lch].lc + dat[rch].lc;
+			else dat[k].lc = dat[lch].lc;
+			if (dat[rch].rc == r - mid) dat[k].rc = dat[lch].rc + dat[rch].rc;
+			else dat[k].rc = dat[rch].rc;
+		} else {
+			dat[k].c = 0;
+			dat[k].lc = dat[lch].lc;
+			dat[k].rc = dat[rch].rc;
 		}
-		// printf("\n");
+		dat[k].c = max(dat[k].c, dat[lch].c);
+		dat[k].c = max(dat[k].c, dat[rch].c);
 	}
-
 }
-int rmq_max(int l, int r) {
-	if (l > r)
+
+int query(int a, int b, int k, int l, int r) {
+	if (r <= a || b <= l)
 		return 0;
-	int k = log((double)(r - l + 1)) / log(2.);
-    // printf("k = %d\n", k);
-	return max(st[l][k], st[r - (1 << k) + 1][k]);
+	else 
+		if (a <= l && r <= b)
+			return dat[k].c;
+		else {
+			int lch = 2 * k + 1, rch = 2 * k + 2, mid = (l + r) / 2;
+			int tmp = max(query(a, b, lch, l, mid), query(a, b, rch, mid, r));
+			int tl = max(l, a), tr = min(r, b);
+			if (tl < mid && mid < tr && A[mid - 1] == A[mid]) {
+				int res = min(dat[lch].rc, mid - tl) + min(dat[rch].lc, tr - mid);
+				tmp = max(tmp, res);
+			}
+			return tmp;
+		}
 }
 
 int main(int argc, char const *argv[])
 {
 	while (scanf("%d", &n) && n) {
 		scanf("%d", &q);
-		for (int i = 1; i <= n; ++i)
+		for (int i = 0; i < n; ++i)
 			scanf("%d", A + i);
-		memset(st, 0, sizeof st);
-		ST();
-		for (int i = 0, l, r; i < q; ++i) {
+		build(0, 0, n);
+		for (int i = 0; i < q; ++i) {
 			scanf("%d%d", &l, &r);
-			int k = l;
-			while (k <= r && A[k] == A[k - 1]) ++k;
-			// cout << l << " " << r << " " << k << endl;
+			printf("%d\n", query(l - 1, r, 0, 0, n));
+		}
+	}
+	return 0;
+}
+
+/* ST
+int n, q, A[MAX_N], ST[MAX_N][20], l, r, ans;
+
+void init() {
+	for (int i = 0; i < n; ++i)
+		if (!i || A[i] != A[i - 1]) ST[i][0] = 1;
+		else ST[i][0] = ST[i - 1][0] + 1;
+	int k = 32 - __builtin_clz(n);
+	for (int j = 1; j < k; ++j)
+		for (int i = 0; i <= n - (1 << j); ++i)
+			ST[i][j] = max(ST[i][j - 1], ST[i + (1 << (j - 1))][j - 1]);
+}
+
+int rmq_max(int l, int r) {
+	if (l >= r) return 0;
+	int k = 31 - __builtin_clz(r - l);
+	return max(ST[l][k], ST[r - (1 << k)][k]);
+}
+
+int main(int argc, char const *argv[])
+{
+	while(scanf("%d", &n) && n) {
+		scanf("%d", &q);
+		for (int i = 0; i < n; ++i)
+			scanf("%d", A + i);
+		memset(ST, 0, sizeof ST);
+		init();
+		for (int i = 0; i < q; ++i) {
+			scanf("%d%d", &l, &r);
+			int k = l--;
+			while (k < r && A[k] == A[k - 1]) ++k;
 			int cnt = rmq_max(k, r);
-			int ans = max(k - l, cnt);
+			ans = max(k - l, cnt);
 			printf("%d\n", ans);
 		}
 	}
 	return 0;
 }
+//*/
