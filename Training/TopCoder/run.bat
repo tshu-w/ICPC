cf5ed1bc4e4aa21bb8a530cbc4fa717d02d64309
Voleking <volekingsg@gmail.com>
2017-09-14 10:53:16 +0800

    Add Topcoder plugin

--- Training/TopCoder/run.bat
+++
@@ -1,1 +0,0 @@
-java -classpath "lib/*" com.topcoder.client.contestApplet.runner.generic www.topcoder.com 5001
--- Training/TopCoder/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -classpath "lib/*" com.topcoder.client.contestApplet.runner.generic www.topcoder.com 5001 >/dev/null 2>&1 &
